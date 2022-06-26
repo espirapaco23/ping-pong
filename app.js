fb4c587fda27b8cb9d9ad5773b204044b50719c6
espirapaco23 <102536675+espirapaco23@users.noreply.github.com>
2022-06-26 11:42:47 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+VANTA.WAVES({
+  el: "#intro",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  // minHeight: 200.0,
+  // minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: 0xe14b4b,
+});
---
+++ appl.js
@@ -0,0 +1,10 @@
+$(function () {
+  $("#reviewsSlider").slick({
+    infinite: true,
+    fade: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+  });
+});
